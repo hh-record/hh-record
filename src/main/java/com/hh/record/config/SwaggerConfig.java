46663fba298ed0e4117e2a37301c4e049bd29836
YuSunjo <tnswh2023@gmail.com>
2022-03-06 11:50:24 +0900

    [sunjo]: swagger-ui config 변경

--- src/main/java/com/hh/record/config/SwaggerConfig.java
+++ src/main/java/com/hh/record/config/SwaggerConfig.java
@@ -6,22 +6,28 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
 
     @Bean
-    public Docket restAPI() {
+    public Docket restApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hh.record"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()));
     }
 
     private ApiInfo apiInfo() {
@@ -30,6 +36,27 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description("HH Record 프로젝트 API 입니다.")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("AUTH_TOKEN", "AUTH_TOKEN", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.ant("/**")).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("/*", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("AUTH_TOKEN", authorizationScopes));
     }
 
 }
