38bbbc643e2e7896cb0562d3bfc1cbb277b4f96b
유순조 <tnswh2023@phpschool.com>
2022-03-21 13:01:33 +0900

    feat(yml): docker

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -4,5 +4,5 @@ DOCKER_IMAGE=ghcr.io/hh-record/hh-record
 
 cat ~/docker_test/deploy/GIT-TOKEN.txt | docker login ghcr.io -u hh-record --password-stdin
 
-docker stop hh-record && docker rm -fv hh-record
+docker stop hh-record && docker rm -fv hh-record && docker rmi -f ghcr.io/hh-record/hh-record:latest
 docker run -d -p 9999:9999 --name hh-record --restart always $DOCKER_IMAGE:latest
