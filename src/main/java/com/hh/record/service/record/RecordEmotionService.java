36bc5b01155c6bde1fe81b15e6b24a153f911026
홍채민 <coals0329@cocen.com>
2022-04-04 13:55:09 +0900

    feat(emotion): add find emotion api and modify emotion entity

--- src/main/java/com/hh/record/service/record/RecordEmotionService.java
+++ src/main/java/com/hh/record/service/record/RecordEmotionService.java
@@ -1,9 +1,14 @@
 package com.hh.record.service.record;
 
 import com.hh.record.dto.record.EmotionRequestDTO;
+import com.hh.record.dto.record.EmotionResponseDTO;
+
+import java.util.List;
 
 public interface RecordEmotionService {
-    void insertEmotion(Long memberId, EmotionRequestDTO requestDTO);
+    void insertEmotion(EmotionRequestDTO requestDTO);
 
     void deleteEmotion(Long memberId, EmotionRequestDTO requestDTO);
+
+    List<EmotionResponseDTO> findByRecordEmotion(Long memberId, Long recordId);
 }
